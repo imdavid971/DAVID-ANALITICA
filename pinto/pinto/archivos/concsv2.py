48c5467b340b9e815afe87477d039ce1b4dcde95
imdavid971 <davidaugustopintomanrique@gmail.com>
2025-04-22 18:26:35 -0500

    csv2.py

--- pinto/pinto/archivos/concsv2.py
+++ pinto/pinto/archivos/concsv2.py
@@ -10,20 +10,4 @@ with open("archivos/employees.csv") as empfile:
     for emp in emplist:
         salarios.append(int(emp[7]))
     
-    print(max(salarios))
-
-
-# import csv
-
-# with open('archivos/employees.csv', 'r') as file:
-#     empleados = [line.strip().split(',') for line in file.readlines()]
-# def obtener_salarios(empleados):
-#     nombres = [
-#         empleados[1] for empleado in empleados[1:] 
-#     ]
-#     salarios = [
-#         float(empleado[7]) for empleado in empleados[1:]
-#         if empleado[7].replace('.', '', 1).isdigit()
-#     ]
-#     return salarios
-# salarios = obtener_salarios(empleados)
+    print(max(salarios)
