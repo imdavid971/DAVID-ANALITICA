53f5c3f3e5308b42b7d8d70038653a310389fe74
imdavid971 <davidaugustopintomanrique@gmail.com>
2025-04-22 18:28:57 -0500

    Actualizar concsv2.py

--- pinto/pinto/archivos/concsv2.py
+++ pinto/pinto/archivos/concsv2.py
@@ -1,13 +1,10 @@
 import csv
 with open("archivos/employees.csv") as empfile:
     data=csv.reader(empfile)
-    #print(type(data))
     emplist=list(data)
-    #print(type(emplist))
-    #print(len(emplist))
     emplist.pop(0)
     salarios=[]
     for emp in emplist:
         salarios.append(int(emp[7]))
     
-    print(max(salarios)
+    print(max(salarios
