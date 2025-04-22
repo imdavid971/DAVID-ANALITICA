55afcceb7d047b746a8431b9b42ec74be6756bb3
imdavid971 <davidaugustopintomanrique@gmail.com>
2025-04-22 18:25:58 -0500

    csv1.py

--- pinto/pinto/archivos/concsv1.py
+++ pinto/pinto/archivos/concsv1.py
@@ -2,7 +2,5 @@ import csv
 
 with open("C:\\Users\\usuario\\Dropbox\\sena2025\\Trimestre1-Febrero10-Abril25\\2996881_Analitica\\datos2.csv") as mycsv:
     data=csv.reader(mycsv)
-    #print(data)
-    #print(type(data))
     for x in data:
         print(x[2])
