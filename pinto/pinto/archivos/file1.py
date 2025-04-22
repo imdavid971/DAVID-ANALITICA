1e7de34d788f1b1d8c653b3a35335edd75b3b430
imdavid971 <davidaugustopintomanrique@gmail.com>
2025-04-22 18:29:43 -0500

    Actualizar file1.py

--- pinto/pinto/archivos/file1.py
+++ pinto/pinto/archivos/file1.py
@@ -1,13 +1,12 @@
-#excepciones
+
 try:
     print(10/0)
 except Exception as fallo:
     print(fallo)
     print("No puedo dividir por cero")
 
-#print("Hola \n Soacha")
 lista=[]
-#stream1=open()
+
 file="archivos\prueba1.txt"
 stream = open(file, mode='w', encoding="utf-8")
 
